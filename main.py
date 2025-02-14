0935424b61b5bfad09505b13500556577190bdab
Jake-M-bmw <47jdmiller@gmail.com>
2025-02-14 12:54:25 -0500

    Updates to code for bookbot

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    return file_contents
 
-main()
+def word_count(text):
+    words = text.split()
+    return(len(words))
+
+def character_count(text):
+    char_count ={}
+    for char in text:
+        char = char.lower()
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+
+
+file_contents = main()
+print(word_count(file_contents))
+print(character_count(file_contents))
