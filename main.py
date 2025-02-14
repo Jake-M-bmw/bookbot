426338a34a51e4bc14e10b97e8392692d6d4abca
Jake-M-bmw <47jdmiller@gmail.com>
2025-02-14 13:19:10 -0500

    finished bookbot project

--- main.py
+++ main.py
@@ -11,14 +11,24 @@ def character_count(text):
     char_count ={}
     for char in text:
         char = char.lower()
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
+        if char.isalpha():
+
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
     return char_count
 
 
 
 file_contents = main()
-print(word_count(file_contents))
-print(character_count(file_contents))
+words = word_count(file_contents)
+char_counts = character_count(file_contents)
+
+
+print(f"--- Begin report of books/frankenstein.txt ---")
+print(f"{words} words found in the document\n")
+for char, count in sorted(char_counts.items()):
+    print(f"The '{char}' character was found {count} times")
+print(f"--- End report ---")
+
